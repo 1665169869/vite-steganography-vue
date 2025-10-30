13182edd37c9ffb928813ff82006e561ca5b7a9e
白羽 <1665169869@qq.com>
2025-10-30 22:55:11 +0800

    ci: 🚀 更新构建配置，添加手动分块和文件命名规则

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 白羽 1665169869@qq.com
  * @Date: 2025-10-30 07:14:14
  * @LastEditors: 白羽 1665169869@qq.com
- * @LastEditTime: 2025-10-30 07:31:29
+ * @LastEditTime: 2025-10-30 22:53:19
  * @FilePath: \vite-steganography-vue\vite.config.ts
  * @Description:
  * Copyright (c) 2025 by 白羽 1665169869@qq.com, All Rights Reserved.
@@ -33,6 +33,23 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          element: ['element-plus'],
+          vue: ['vue'],
+          utils: ['lodash-es'],
+          zip: [
+            '@zip.js/zip.js',
+          ]
+        },
+        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+        assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+      },
     },
   },
 })
