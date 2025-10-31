7d1a9fe5e6937cc249b72954a34cd428f9b4db70
白羽 <1665169869@qq.com>
2025-11-01 05:23:44 +0800

    ci(vite-config): 👷 合并手动分块配置以优化打包策略 将原本分散的多个 manualChunks 配置项合并为一个统一的 chunk， 包含 vue、element-plus、lodash-es 和 @zip.js/zip.js， 简化了 rollup 打包配置逻辑。

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 白羽 1665169869@qq.com
  * @Date: 2025-10-30 07:14:14
  * @LastEditors: 白羽 1665169869@qq.com
- * @LastEditTime: 2025-10-30 22:53:19
+ * @LastEditTime: 2025-11-01 05:22:29
  * @FilePath: \vite-steganography-vue\vite.config.ts
  * @Description:
  * Copyright (c) 2025 by 白羽 1665169869@qq.com, All Rights Reserved.
@@ -39,12 +39,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          element: ['element-plus'],
-          vue: ['vue'],
-          utils: ['lodash-es'],
-          zip: [
-            '@zip.js/zip.js',
-          ]
+          chunk: ['vue', 'element-plus', 'lodash-es', '@zip.js/zip.js',]
         },
         chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
         entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
